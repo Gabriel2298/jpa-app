7944ae85949374fcdbd4d298d03b6304d23f09cd
Palacean Samuel <samy.palacean@gmail.com>
2024-11-01 22:23:29 +0200

    Update User.java

--- src/main/java/com/itscool/jpa/models/User.java
+++ src/main/java/com/itscool/jpa/models/User.java
@@ -20,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer age;
